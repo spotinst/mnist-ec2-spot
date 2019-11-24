5a39a029ae3550bc67bd586a08569864a60444a2
itay.ess <itay.ess@spotinst.com>
2019-11-24 23:25:12 +0200

    [src] fix

--- userdata/data_persistency_userdata.sh
+++ userdata/data_persistency_userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-EC2_REGION="$(echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:')"
+EC2_REGION= "`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 DEVICE_NAME="/dev/xvdb"
 DATA_STATE="unknown"
 
@@ -35,7 +35,7 @@ mkdir -p models
 mkdir -p ~/ec2-spot-labs
 git clone https://github.com/essale/mnist-ec2-spot.git
 chown -R ubuntu: mnist-ec2-spot
-cd ~/ec2-spot-labs/scripts/
+cd ~/ec2-spot-labs/mnist-ec2-spot/scripts/
 
 # Download dataset if not already downloaded before
 [ "$(ls -A /dl/datasets/)" ] && echo "Not Empty" || curl -o /dl/datasets/mnist.npz https://s3.amazonaws.com/img-datasets/mnist.npz
